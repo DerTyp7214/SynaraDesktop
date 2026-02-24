19e439720efec82f812befa3185bb5c9fb3697c5
DerBot7214 <github@dertyp.dev>
2026-02-24 17:56:26 +0100

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kotlin.compose)
+}
+
+val osName = System.getProperty("os.name").lowercase()
+val targetNatives = when {
+    osName.contains("win") -> "natives-windows"
+    osName.contains("mac") -> "natives-macos"
+    else -> "natives-linux"
+}
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation(libs.runtime)
+                implementation(libs.foundation)
+                implementation(libs.material3)
+                implementation(libs.ui)
+                implementation(libs.components.resources)
+                implementation(libs.kotlinx.coroutines.core)
+                
+                implementation(libs.lwjgl)
+                implementation(libs.lwjgl.glfw)
+                implementation(libs.lwjgl.opengl)
+                implementation(libs.lwjgl.stb)
+
+                runtimeOnly("org.lwjgl:lwjgl::$targetNatives")
+                runtimeOnly("org.lwjgl:lwjgl-glfw::$targetNatives")
+                runtimeOnly("org.lwjgl:lwjgl-opengl::$targetNatives")
+                runtimeOnly("org.lwjgl:lwjgl-stb::$targetNatives")
+
+                implementation(project(":common-rpc"))
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "dev.dertyp.synara.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-rpc/maven")
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-rpc/maven")
+    }
+}
+
+rootProject.name = "Synara"
+include(":app")
+include(":common-rpc")
+include(":common-rpc:compiler")
